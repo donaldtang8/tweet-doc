7b1ac3e063a50afcc70a73649a06822ad11a0e08
Donald Tang <turazinyc@gmail.com>
2019-11-28 01:28:47 -0500

    Configured for deployment

--- server.js
+++ server.js
@@ -46,8 +46,11 @@ function getSentiment(req, res) {
   // res.send(results);
 }
 
-// Creates a server which runs on port 3000 and
-// can be accessed through localhost:3000
-app.listen(3000, (req, res) => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, (req, res) => {
   console.log("server running on port 3000");
 });
